ffcf570f06ef7fdde0b4b0b83a60568c796e6a84
Tesla2000 <fratajczak124@gmail.com>
2022-01-22 17:06:54 +0100

    Dodałem pole wiek i płeć do pacjenta i metody umożliające dostęp do nich

--- src/main/java/edu/ib/Patient.java
+++ src/main/java/edu/ib/Patient.java
@@ -1,5 +1,6 @@
 package edu.ib;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Patient {
@@ -7,6 +8,7 @@ public class Patient {
     private Long id;
     private Long phoneNumber = null;
     private String password;
+    private String gender;
     private ArrayList<Patient> permissions = new ArrayList<>();
 
     public Patient(String name, Long id, Long phoneNumber, String password) {
@@ -14,6 +16,8 @@ public class Patient {
         this.id = id;
         this.phoneNumber = phoneNumber;
         this.password = password;
+        if (id % 2 == 1) this.gender = "Man";
+        else this.gender = "Woman";
     }
 
     public Patient(String name, Long id, String password) {
@@ -21,8 +25,48 @@ public class Patient {
         this.id = id;
         this.password = password;
     }
-    public void addPermission(Patient patient){
+
+    public void addPermission(Patient patient) {
         permissions.add(patient);
+    }
+
+    public void revokePermission(Patient patient){
+        permissions.remove(patient);
+    }
+
+    public boolean isAdult() {
+        int year = LocalDateTime.now().getYear();
+        int month = LocalDateTime.now().getMonth().getValue();
+        int day = LocalDateTime.now().getDayOfMonth();
+        int yearOfBirth;
+        if (id % 1000000000 > 200000000)
+            yearOfBirth = 2000 + (int) (id / 100000000);
+        else
+            yearOfBirth = 1900 + (int) (id / 100000000);
+        int monthOfBirth = (int) ((id % 200000000) / 10000000);
+        int dayOfBirth = (int) ((id % 10000000) / 100000);
+        int age = year - yearOfBirth;
+        if (monthOfBirth > month) age -= 1;
+        else if (monthOfBirth==month && dayOfBirth > day) age -= 1;
+        return age >= 18;
+    }
+
+
+    public int getAgeAtVaccination(LocalDateTime vaccinationDate) {
+        int year = vaccinationDate.getYear();
+        int month = vaccinationDate.getMonth().getValue();
+        int day = vaccinationDate.getDayOfMonth();
+        int yearOfBirth;
+        if (id % 1000000000 > 200000000)
+            yearOfBirth = 2000 + (int) (id / 100000000);
+        else
+            yearOfBirth = 1900 + (int) (id / 100000000);
+        int monthOfBirth = (int) ((id % 200000000) / 10000000);
+        int dayOfBirth = (int) ((id % 10000000) / 100000);
+        int age = year - yearOfBirth;
+        if (monthOfBirth > month) age -= 1;
+        else if (monthOfBirth==month && dayOfBirth > day) age -= 1;
+        return age;
     }
 
     public ArrayList<Patient> getPermissions() {
@@ -61,5 +105,7 @@ public class Patient {
         this.password = password;
     }
 
-
+    public String getGender() {
+        return gender;
+    }
 }
