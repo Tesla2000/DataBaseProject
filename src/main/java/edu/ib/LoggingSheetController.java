15e5bb9b8a543abf65e8e4267277135a85e5d986
Tesla2000 <fratajczak124@gmail.com>
2022-01-22 22:36:43 +0100

    Dodałem pole informujące o niepowodzeniu logowania

--- src/main/java/edu/ib/LoggingSheetController.java
+++ src/main/java/edu/ib/LoggingSheetController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class LoggingSheetController {
 
@@ -21,6 +22,9 @@ public class LoggingSheetController {
 
     @FXML
     private Button RegisterButton;
+
+    @FXML
+    private Text loggerResponse;
 
     @FXML
     private TextField loggingField;
@@ -42,7 +46,8 @@ public class LoggingSheetController {
     void initialize() {
         assert LogInButton != null : "fx:id=\"LogInButton\" was not injected: check your FXML file 'loggingSheet.fxml'.";
         assert RegisterButton != null : "fx:id=\"RegisterButton\" was not injected: check your FXML file 'loggingSheet.fxml'.";
-        assert loggingField != null : "fx:id=\"logginField\" was not injected: check your FXML file 'loggingSheet.fxml'.";
+        assert loggerResponse != null : "fx:id=\"loggerResponse\" was not injected: check your FXML file 'loggingSheet.fxml'.";
+        assert loggingField != null : "fx:id=\"loggingField\" was not injected: check your FXML file 'loggingSheet.fxml'.";
         assert passwordField != null : "fx:id=\"passwordField\" was not injected: check your FXML file 'loggingSheet.fxml'.";
 
     }
