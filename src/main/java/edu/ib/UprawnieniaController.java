f0ce8e98b3b3434f485c588b6d2b896a9bced326
Michał Doliński <olinskimichal1@gmail.com>
2022-01-30 10:24:59 +0100

    dodawanie uprawnień

--- src/main/java/edu/ib/UprawnieniaController.java
+++ src/main/java/edu/ib/UprawnieniaController.java
@@ -1,7 +1,11 @@
 package edu.ib;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import edu.ib.structures.Tester;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -34,5 +38,22 @@ public class UprawnieniaController {
         assert zatwierdzUprawnieniaButton != null : "fx:id=\"zatwierdzUprawnieniaButton\" was not injected: check your FXML file 'uprawnienia.fxml'.";
 
     }
+
+    @FXML
+    void anulujDodawanie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zatwierdz(ActionEvent event) throws SQLException {
+        String pierwszy = peselPierwszyTextField.getText();
+        String drugi = peselDwaTextField.getText();
+        Tester.callProcedure("dodawanie_uprawnien("+pierwszy+","+drugi+")");
+
+    }
+
+
+
+
 
 }
